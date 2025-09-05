323d70cb93c8954e77030c1656062375f1209edc
Kin Fai Tse <kftse20031207@gmail.com>
2025-09-05 14:06:55 +0800

    fix v1 version (#7)
    Co-authored-by: kftse-ust-hk <kftse@ust.hk>

--- var/spack/repos/builtin/packages/jasper/package.py
+++ var/spack/repos/builtin/packages/jasper/package.py
@@ -32,11 +32,7 @@ class Jasper(AutotoolsPackage, CMakePackage):
     version("2.0.31", sha256="d419baa2f8a6ffda18472487f6314f0f08b673204723bf11c3a1f5b3f1b8e768")
     version("2.0.16", sha256="f1d8b90f231184d99968f361884e2054a1714fdbbd9944ba1ae4ebdcc9bbfdb1")
     version("2.0.14", sha256="85266eea728f8b14365db9eaf1edc7be4c348704e562bb05095b9a077cf1a97b")
-    version(
-        "1.900.1",
-        sha256="c2b03f28166f9dc8ae434918839ae9aa9962b880fcfd24eebddd0a2daeb9192c",
-        deprecated=True,
-    )
+    version("1.900.31", sha256="c2b03f28166f9dc8ae434918839ae9aa9962b880fcfd24eebddd0a2daeb9192c")
 
     build_system(
         conditional("cmake", when="@2:"), conditional("autotools", when="@:1"), default="cmake"
